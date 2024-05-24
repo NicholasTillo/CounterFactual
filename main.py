14ec733a9a07e1c0812887fb8c6596200c7d5854
Nicholas Tillo <nicholas.tillo@gmail.com>
2024-05-24 10:41:29 -0400

    Auto

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 
-#!/usr/bin/env python3
-#    This file is part of qdpy.
-#
-#    qdpy is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of
-#    the License, or (at your option) any later version.
-#
-#    qdpy is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with qdpy. If not, see <http://www.gnu.org/licenses/>.
-import qdpy
+import math
+from qdpy import algorithms, containers, benchmarks, plots
+
+grid = containers.Grid(shape=(64,64), max_items_per_bin=1, fitness_domain=((0., 1.),), features_domain=((0., 1.), ( 0., 1.)))
+#What do these features mean? 
+#:SOB:
+
+
+algo = algorithms.RandomSearchMutPolyBounded(grid, budget=100000, batch_size=500, dimension=3, optimisation_task="maximization")
+logger = algorithms.TQDMAlgorithmLogger(algo)
+
+
+def eval_fn(ind):
+    """An example evaluation function. It takes an individual as input, and returns the pair ``(fitness, features)``, where ``fitness`` and ``features`` are sequences of scores."""
+    normalization = sum((x for x in ind))
+    k = 10.
+    score = 1. - sum(( math.cos(k * ind[i]) * math.exp(-(ind[i]*ind[i])/2.) for i in range(len(ind)))) / float(len(ind))
+    fit0 = sum((x * math.sin(abs(x) * 2. * math.pi) for x in ind)) / normalization
+    fit1 = sum((x * math.cos(abs(x) * 2. * math.pi) for x in ind)) / normalization
+    features = (fit0, fit1)
+    return (score,), features
+
+
+#What are the individuals though? What makes them
+best = algo.optimise(eval_fn)
+print("\n" + algo.summary())
+plots.default_plots_grid(logger)
